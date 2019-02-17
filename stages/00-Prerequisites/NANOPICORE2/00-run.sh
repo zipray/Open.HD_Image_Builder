446b54c62bbdbfdae24c218a808f99ae1f1b9cbf
Jelle Tigchelaar <RespawnDespair@users.noreply.github.com>
2019-02-17 20:09:09 +0100

    Wrong filepath

--- stages/00-Prerequisites/NANOPICORE2/00-run.sh
+++ stages/00-Prerequisites/NANOPICORE2/00-run.sh
@@ -5,7 +5,7 @@ pushd "${STAGE_DIR}"
 if [ ! -d ~/nanopitools ]; then
     log "Extract NanoPi Tools"
     mkdir -p "~/nanopitools"
-    tar xf "gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu.tar.xz" -C "~/nanopitools"
+    tar xf "gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu.tar.xz" -C ~/nanopitools
 
     log "Install the NanoPi Tools"
     echo PATH=\$PATH:~/nanopitools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin >> ~/.bashrc
